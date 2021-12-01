fc0a5f9e959cc406c4bbc7c5054ba9f9bfb93f63
LucasV <LucasV@pc-32.home>
2021-12-01 15:57:39 +0100

    Les au lieu de la

--- src/test/compteur.java
+++ src/test/compteur.java
@@ -3,7 +3,7 @@ public class compteur {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		int i;
-		System.out.println("La tables de multiplications de " + 5);
+		System.out.println("Les tables de multiplications de " + 5);
 		for (i = 1; i <= 10; i++)
 			System.out.println(5 + "x" + i + " = " + (5 * i));
 		
