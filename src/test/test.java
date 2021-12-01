424cecce67be9968d3929d97f7de3e6b9c12bfa3
LucasV <LucasV@pc-32.home>
2021-12-01 15:40:17 +0100

    v2

--- src/test/test.java
+++ src/test/test.java
@@ -1,12 +1,10 @@
 package test;
-
 public class test {
-
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		// TODO Auto-generated method stub
-
-		
-		System.out.println("hello world");
+		int i;
+		System.out.println("La table de multiplication de " + 5);
+		for (i = 1; i <= 10; i++)
+			System.out.println(5 + "x" + i + " = " + (5 * i));
+		}
 	}
-
-}
